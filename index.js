0a2f3d567e639b2a635e8c1db58c6a314c097f2b
Jordan <jordankleinbaum1@gmail.com>
2023-08-09 02:25:53 -0400

    Added DOMContentLoaded to JS File

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const questions = document.querySelectorAll(".question_and_carrot");
+this.addEventListener("DOMContentLoaded", () => {
+  const questions = document.querySelectorAll(".question_and_carrot");
 
-questions.forEach((question) => {
-  const answer = question.nextElementSibling;
-  const carrot = question.querySelector(".carrot");
+  questions.forEach((question) => {
+    const answer = question.nextElementSibling;
+    const carrot = question.querySelector(".carrot");
 
-  question.addEventListener("click", () => {
-    if (answer.style.maxHeight) {
-      answer.style.overflow = "hidden";
-      answer.style.maxHeight = null;
-      question.style.fontWeight = "normal";
-      carrot.style.transform = "rotate(0deg)";
-    } else {
-      answer.style.maxHeight = answer.scrollHeight + "px";
-      question.style.fontWeight = "bold";
-      carrot.style.transform = "rotate(180deg)";
-    }
+    question.addEventListener("click", () => {
+      if (answer.style.maxHeight) {
+        answer.style.overflow = "hidden";
+        answer.style.maxHeight = null;
+        question.style.fontWeight = "normal";
+        carrot.style.transform = "rotate(0deg)";
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        question.style.fontWeight = "bold";
+        carrot.style.transform = "rotate(180deg)";
+      }
+    });
   });
 });
